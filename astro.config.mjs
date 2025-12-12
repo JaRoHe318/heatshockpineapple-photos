eb1c74234c644b9f799f361772d89c89278189f9
JaRoHe318 <jarohe1000@gmail.com>
2025-12-11 18:39:35 -0800

    fixing social card

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  site: "https://photos.heatshockpineapple.com",
   // IMPORTANT: no `base: '/code'` here.
   // If you see a base property, delete it or set it to '/'.
+  site: "https://photos.heatshockpineapple.com",
+
+  integrations: [sitemap()]
 });
